27286d734d9a15a5bca5a1b145b25a4a414ff290
nauka117 <v_kozyrev@outlook.com>
2025-02-12 23:01:29 +0300

    fix: fixed request_and_save function

--- src/data_loading/loader.py
+++ src/data_loading/loader.py
@@ -2,6 +2,11 @@ from src.data_loading.weather_requests import (
     HistoryWeatherRequestBase
 )
 
+import os
+from pathlib import Path
+
+import json
+
 
 def request_and_save(request: HistoryWeatherRequestBase):
     directories = {
@@ -15,8 +20,14 @@ def request_and_save(request: HistoryWeatherRequestBase):
 
     response = request.query()
 
+    project_root = Path(__file__).parent.parent.parent
     cur_dir = directories[request.__class__.__name__]
 
+    save_dir = project_root / "data" / "raw" / cur_dir
+    os.makedirs(save_dir, exist_ok=True)
 
-    with open(f"src/data_loader/data/{cur_dir}/{request.startDateTime}.json", "w") as f:
-        f.write(response.text)
+    file_name = f"{request.startDateTime['unix']}.json"
+    file_path = save_dir / file_name
+
+    with open(file_path, "w") as f:
+        json.dump(response, f, indent=4)
